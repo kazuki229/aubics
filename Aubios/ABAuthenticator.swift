004131e91ae0c842e2430005593b1a36bdce4c98
kazuki229 <tsuzuku.k@gmail.com>
2018-02-20 00:25:35 +0900

    add: travis.yml,proj,framework

---
+++ Aubios/ABAuthenticator.swift
@@ -0,0 +1,18 @@
+//
+//  ABAuthenticator.swift
+//  Aubios
+//
+//  Created by 都筑一希 on 2018/02/19.
+//  Copyright © 2018年 kazuki229. All rights reserved.
+//
+
+import UIKit
+
+public class ABAuthenticator {
+    public init() {
+        
+    }
+    public func hoge() -> String {
+        return "hoge"
+    }
+}
---
+++ Aubios/Aubios.h
@@ -0,0 +1,19 @@
+//
+//  Aubios.h
+//  Aubios
+//
+//  Created by 都筑一希 on 2018/02/19.
+//  Copyright © 2018年 kazuki229. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Aubios.
+FOUNDATION_EXPORT double AubiosVersionNumber;
+
+//! Project version string for Aubios.
+FOUNDATION_EXPORT const unsigned char AubiosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Aubios/PublicHeader.h>
+
+
